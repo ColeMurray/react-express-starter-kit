41bf208e456eb277a5f2cbb4ba32561873034d12
Cole Murray <colemurray.cs@gmail.com>
2016-06-22 21:42:37 -0700

    Add Production Flag and Port

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 
 const app = express();
+const DEBUG = process.env.NODE_ENV !== 'production';
+const PORT = DEBUG ? '3000' : process.env.PORT;
 
 app.use(express.static(__dirname + '/../../public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const server = app.listen(3000, function () {
+const server = app.listen(PORT, function () {
     console.log('Express listening on port 3000');
 });
